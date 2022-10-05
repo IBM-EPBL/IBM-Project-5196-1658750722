449732d9c011704a958f42c91429f809e198ec1b
ROSHANAZAR <77892624+ROSHANAZAR@users.noreply.github.com>
2022-10-05 12:55:47 +0530

    Update app.py

--- Assessments/ROSHAN(211419205138)/Assignment3/db2,object storage,watson/app.py
+++ Assessments/ROSHAN(211419205138)/Assignment3/db2,object storage,watson/app.py
@@ -4,11 +4,10 @@ import ibm_boto3
 from ibm_botocore.client import Config, ClientError
 import ibm_db
 
-COS_ENDPOINT = "https://s3.jp-tok.cloud-object-storage.appdomain.cloud"
-COS_API_KEY_ID = "u1bzkbyrggJz-7j0SpDuxU9-k9vpFJT_E8InjdEPWyNh"
-COS_INSTANCE_CRN = "crn:v1:bluemix:public:cloud-object-storage:global:a/e3b268b531d74ad5807836061785a60d:ce50c4ae-b19e-4dbd-a6c3-23656dcb8051:bucket:funedu"
+COS_ENDPOINT = 
+COS_API_KEY_ID = 
+COS_INSTANCE_CRN = 
 
-# Create resource https://s3.ap.cloud-object-storage.appdomain.cloud
 cos = ibm_boto3.resource("s3",
                          ibm_api_key_id=COS_API_KEY_ID,
                          ibm_service_instance_id=COS_INSTANCE_CRN,
@@ -19,8 +18,7 @@ cos = ibm_boto3.resource("s3",
 
 
 conn = ibm_db.connect(
-    "DATABASE=bludb;HOSTNAME=fbd88901-ebdb-4a4f-a32e-9822b9fb237b.c1ogj3sd0tgtu0lqde00.databases.appdomain.cloud;PORT=32731;SECURITY=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt;UID=spk61607;PWD=yWm4ioBhz9v4QJ0W",
-    '', '')
+    )
 def get_bucket_contents(bucket_name):
     print("Retrieving bucket contents from: {0}".format(bucket_name))
     try:

468c7da3ac54c51f63584b5d65a210d049d94f80
ROSHANAZAR <77892624+ROSHANAZAR@users.noreply.github.com>
2022-11-17 19:53:43 +0530

    Update key_search.py

--- Project Development Phase/Sprint 3/key_search.py
+++ Project Development Phase/Sprint 3/key_search.py
@@ -6,7 +6,7 @@ def search_by_key(keyword):
     querystring = {"q":keyword,"lang":"en","sort_by":"relevancy","page":"1","media":"True"}
 
     headers = {
-	    "X-RapidAPI-Key": "dcaf96a863msh352fd632acebebfp19f2d0jsn74b519ba2e84",
+	    "X-RapidAPI-Key": "",
 	    "X-RapidAPI-Host": "newscatcher.p.rapidapi.com"
     }
 
